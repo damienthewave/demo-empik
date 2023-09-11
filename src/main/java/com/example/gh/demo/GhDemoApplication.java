073495ff273e983f3a0a72e96c7e665f400e4a9c
damienthewave <garbalad@gmail.com>
2023-09-11 10:29:32 +0200

    Init

---
+++ src/main/java/com/example/gh/demo/GhDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhDemoApplication.class, args);
+    }
+
+}
